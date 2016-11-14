b5ca6aef5173c40d4c83bdd3094788e6b406e5ab
Bill St. Clair <billstclair@gmail.com>
2016-11-14 15:34:05 -0500

    Upgrade to Elm 0.18.

--- VersionedJson.elm
+++ VersionedJson.elm
@@ -12,27 +12,34 @@ See the README for the [examples](https://github.com/billstclair/elm-versioned-j
 -}
 
 import Dict exposing (Dict)
-import Json.Decode as JD exposing ((:=))
+import Json.Decode as JD exposing (field)
 import Json.Encode as JE
 
-{-| Convert a value to a versioned Json string -}
+
+{-| Convert a value to a versioned Json string
+-}
 encodeVersionedJson : Int -> x -> (x -> String) -> String
 encodeVersionedJson version value encoder =
-  let plist = [ ("version", JE.int version)
-              , ("value", JE.string <| encoder value)
-              ]
-  in
-    JE.encode 0 <| JE.object plist
+    let
+        plist =
+            [ ( "version", JE.int version )
+            , ( "value", JE.string <| encoder value )
+            ]
+    in
+        JE.encode 0 <| JE.object plist
 
-{-| An Elm Dict mapping version numbers to decoder functions -}
+
+{-| An Elm Dict mapping version numbers to decoder functions
+-}
 type alias ConverterDict x =
-  Dict Int (String -> Result String x)
+    Dict Int (String -> Result String x)
 
-versionAndValue : JD.Decoder (Int, String)
+
+versionAndValue : JD.Decoder ( Int, String )
 versionAndValue =
-  JD.object2 (,)
-    ("version" := JD.int)
-    ("value" := JD.string)
+    JD.map2 (,)
+        (field "version" JD.int)
+        (field "value" JD.string)
 
 {-| Decode a string saved by `encodeVersionedJson` with the relavant converter function
 from a dictionary you provide.
@@ -42,12 +49,19 @@ for version 0.
 -}
 decodeVersionedJson : String -> ConverterDict x -> Result String x
 decodeVersionedJson json dict =
-  case JD.decodeString versionAndValue json of
-    Err _ ->
-      case Dict.get 0 dict of
-        Nothing -> Err "No converter for version 0."
-        Just c -> c json
-    Ok (version, value) ->
-      case Dict.get version dict of
-        Nothing -> Err ("No converter for version " ++ toString(version))
-        Just c -> c value
+    case JD.decodeString versionAndValue json of
+        Err _ ->
+            case Dict.get 0 dict of
+                Nothing ->
+                    Err "No converter for version 0."
+
+                Just c ->
+                    c json
+
+        Ok ( version, value ) ->
+            case Dict.get version dict of
+                Nothing ->
+                    Err ("No converter for version " ++ toString (version))
+
+                Just c ->
+                    c value
